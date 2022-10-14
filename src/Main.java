1b41b68801a50396c3895ee755c20e373bdf899b
1E10 Laptop <sample@bths.edu>
2022-10-14 10:54:35 -0400

    2

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        /*Circle circle = new Circle(5.0);
+        System.out.println(circle.getInfo());
+        circle.setRadius(9.2);
+        System.out.println(circle.getInfo());
+
+        circle.setRadius(5.8);
+        System.out.println(circle.getInfo());
+        circle.setRadius(2.0);
+        System.out.println(circle.getInfo());
+
+        RightTriangle triangle = new RightTriangle(3,4);
+        RightTriangle triangle2 = new RightTriangle(6.5,10.7);
+        double hypotenuse1 = triangle.hypotenuse();
+        double hypotenuse2 = triangle2.hypotenuse();
+        System.out.println(hypotenuse1);
+        System.out.println(hypotenuse2);
+
+        System.out.println(Math.random());
+        System.out.println(Math.random());
+        System.out.println(Math.random());
+        System.out.println(Math.random());*/
+
+        LuckyNumbers numbers = new LuckyNumbers();
+        System.out.println(numbers.getLuckyNumbers());
+        System.out.println(numbers.getLuckyNumbers());
+    }
+}
